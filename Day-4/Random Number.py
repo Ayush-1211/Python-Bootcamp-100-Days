ce4b63892b27ef0570bc788aeaaa406743461471
Ayush Prajapati <63777803+Ayush-1211@users.noreply.github.com>
2022-03-15 18:23:15 +0530

    1

---
+++ Day-4/Random Number.py
@@ -0,0 +1,10 @@
+import random
+
+random_int = random.randint(1,10)
+print(random_int)
+
+random_float = random.random()  # 0-1
+print(random_float)
+
+random_float2 = random.random() * 10    # 0-10
+print(random_float2)
