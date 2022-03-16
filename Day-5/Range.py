12cca1303932076aa82d18769c3f58f727853c5e
Ayush Prajapati <63777803+Ayush-1211@users.noreply.github.com>
2022-03-16 16:36:16 +0530

    4

---
+++ Day-5/Range.py
@@ -0,0 +1,14 @@
+for number in range(1,11):
+    print(number)
+
+print("---------------")
+
+for i in range(1,11,3):
+    print(i)
+
+print("---------------")
+
+sum = 0
+for num in range(1,101):
+    sum += num
+print(sum)
