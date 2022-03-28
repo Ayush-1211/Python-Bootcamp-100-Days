1d8715f47fe5dc3a968184a2f5af4ee2760bd8d0
Ayush Prajapati <63777803+Ayush-1211@users.noreply.github.com>
2022-03-28 10:48:10 +0530

    2

---
+++ Day-17/init.py
@@ -0,0 +1,13 @@
+class User:
+    def __init__(self):
+        print("User being created!!")
+
+user_1 = User()
+user_1.id = "1"
+user_1.name = "Ayush"
+print(user_1.name)
+
+user_2 = User()
+user_2.id = "2"
+user_2.name = "Anand"
+print(user_2.name)
