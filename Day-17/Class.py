a1a0abb1ab53567807f3cfdcc8e2f6a426333664
Ayush Prajapati <63777803+Ayush-1211@users.noreply.github.com>
2022-03-28 09:27:44 +0530

    1

---
+++ Day-17/Class.py
@@ -0,0 +1,12 @@
+class User():
+    pass
+
+user_1 = User()     # Class: User, Object: user_1
+user_1.id = "1"     # Attribute: id
+user_1.name = "Ayush"
+print(user_1.name)
+
+user_2 = User()
+user_2.id = "2"
+user_2.name = "Anand"
+print(user_2.name)
